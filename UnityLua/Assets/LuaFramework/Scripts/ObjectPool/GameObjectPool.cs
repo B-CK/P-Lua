6de0987f06b5bbd635b2182d92d9789c56775148
瞎蹦跶 <609477964@qq.com>
2018-07-04 19:16:31 +0800

    asset
    对象池以及资源加载

--- UnityLua/Assets/LuaFramework/Scripts/ObjectPool/GameObjectPool.cs
+++ UnityLua/Assets/LuaFramework/Scripts/ObjectPool/GameObjectPool.cs
@@ -3,67 +3,99 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 
-namespace LuaFramework {
+namespace LuaFramework
+{
+    public class GameObjectItem
+    {
+        public GameObject prefab;
+        public float lastUsedTime;
+    }
 
-	[Serializable]
-	public class PoolInfo {
-		public string poolName;
-		public GameObject prefab;
-		public int poolSize;
-		public bool fixedSize;
-	}
-
-	public class GameObjectPool {
+    public class GameObjectPool
+    {
         private int maxSize;
-		private int poolSize;
-		private string poolName;
+        private string poolName;
         private Transform poolRoot;
         private GameObject poolObjectPrefab;
         private Stack<GameObject> availableObjStack = new Stack<GameObject>();
 
-        public GameObjectPool(string poolName, GameObject poolObjectPrefab, int initCount, int maxSize, Transform pool) {
-			this.poolName = poolName;
-			this.poolSize = initCount;
+
+        public GameObjectPool(string poolName, GameObject poolObjectPrefab, int maxSize, Transform pool)
+        {
+            this.poolName = poolName;
             this.maxSize = maxSize;
             this.poolRoot = pool;
             this.poolObjectPrefab = poolObjectPrefab;
+        }
 
-			//populate the pool
-			for(int index = 0; index < initCount; index++) {
-				AddObjectToPool(NewObjectInstance());
-			}
-		}
+        public void SetDefaultCount(int count)
+        {
+            for (int i = 0; i < count; i++)
+            {
+                AddObjectToPool(NewObjectInstance());
+            }
+        }
 
-		//o(1)
-        private void AddObjectToPool(GameObject go) {
-			//add to pool
+        //o(1)
+        private void AddObjectToPool(GameObject go)
+        {
+            //add to pool
             go.SetActive(false);
             availableObjStack.Push(go);
             go.transform.SetParent(poolRoot, false);
-		}
+        }
 
-        private GameObject NewObjectInstance() {
+        private GameObject NewObjectInstance()
+        {
             return GameObject.Instantiate(poolObjectPrefab) as GameObject;
-		}
+        }
 
-		public GameObject NextAvailableObject() {
+        public GameObject Get()
+        {
             GameObject go = null;
-			if(availableObjStack.Count > 0) {
-				go = availableObjStack.Pop();
-			} else {
-				Debug.LogWarning("No object available & cannot grow pool: " + poolName);
-			}
+            if (availableObjStack.Count > 0)
+            {
+                go = availableObjStack.Pop();
+            }
+            else
+            {
+                Debug.LogWarning("No object available & cannot grow pool: " + poolName);
+            }
             go.SetActive(true);
             return go;
-		} 
-		
-		//o(1)
-        public void ReturnObjectToPool(string pool, GameObject po) {
-            if (poolName.Equals(pool)) {
+        }
+        //o(1)
+        public void Put(string pool, GameObject po)
+        {
+            if (poolName.Equals(pool))
+            {
                 AddObjectToPool(po);
-			} else {
-				Debug.LogError(string.Format("Trying to add object to incorrect pool {0} ", poolName));
-			}
-		}
-	}
+            }
+            else
+            {
+                Debug.LogError(string.Format("Trying to add object to incorrect pool {0} ", poolName));
+            }
+        }
+
+
+
+        //定时清理功能
+        private float cleanInterval = -1;
+        private float cacheTime = -1;
+
+        public void OpenCleanFunc(float interval, float cacheTime)
+        {
+            this.cleanInterval = interval;
+            this.cacheTime = cacheTime;
+            //添加到资源管理器中,等待清理
+            //TODO
+        }
+        public void CloseCleanFunc()
+        {
+            this.cleanInterval = -1;
+            this.cacheTime = -1;
+            //移除清理功能
+            //TODO
+        }
+    }
 }
